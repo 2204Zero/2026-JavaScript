f9f0494e2a0c22a35eeb30693b86c5068307c0b9
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2026-01-03 21:16:57 +0530

    data types in js

---
+++ Day-02.js
@@ -0,0 +1,26 @@
+// DataTypes in Js
+"use strict";              // treat all js code as newer version
+
+let name = "Zero"
+console.log(typeof name);
+let age = 19
+console.log(typeof age);
+let weight = 60.5
+console.log(typeof weight);               // there is no float type in JS - it will also be treated as number
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+
+let a = null                            // null is a standalone value & data type 
+console.log(typeof a);                      // type of null is object
+
+let c
+console.log(typeof c);                  // return undefined
+
+console.log(typeof undefined);          // return undefined     
+
+
+// symbols - used in figma and all for uniqueness
+
+
+// console.table(typeof[name,age,weight])       this will print object
+
